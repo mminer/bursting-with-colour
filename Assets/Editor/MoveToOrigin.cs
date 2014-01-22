e78d1e4b0dd15c0003dfa56ca14773b9321f4f0f
Matthew Miner <matthew@matthewminer.com>
2014-01-22 11:41:58 -0500

    Add Move To Origin editor script.

---
+++ Assets/Editor/MoveToOrigin.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEngine;
+
+static class MoveToOrigin
+{
+	/// <summary>
+	/// Moves selected game object(s) to (0, 0, 0).
+	/// Keyboard shortcut: shift-cmd-0 (Mac), shift-ctrl-0 (Windows).
+	/// <summary>
+	[MenuItem ("GameObject/Move To Origin #%0")]
+	static void MenuMoveToOrigin ()
+	{
+		foreach (var t in Selection.transforms) {
+			Undo.RecordObject(t, "Move " + t.name);
+			t.position = Vector3.zero;
+			Debug.Log("Moving " + t.name + " to origin.");
+		}
+	}
+
+	/// <summary>
+	/// Enables "Move To Origin" menu item when a transform is selected.
+	/// </summary>
+	[MenuItem ("GameObject/Move To Origin #%0", true)]
+	static bool ValidateMoveToOrigin ()
+	{
+		return Selection.activeTransform != null;
+	}
+}
