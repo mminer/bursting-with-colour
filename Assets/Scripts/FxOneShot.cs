616598d6e3b710dd9f260c0774b1c36e928ad27c
brentgmitchell <brentgmitchell@outlook.com>
2014-01-25 16:02:08 -0500

    Adding kill fx and camera tweaks

---
+++ Assets/Scripts/FxOneShot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FxOneShot : MonoBehaviour {
+
+	void Start()
+	{
+		particleSystem.renderer.sortingLayerName = "Foreground";
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!particleSystem.isPlaying)
+			Destroy (gameObject);
+	}
+}
