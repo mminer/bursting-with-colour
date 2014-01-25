d7d1508eaee1a37283057d495ba28462dbcdd74d
Brad Keys <brad@bradkeys.com>
2014-01-25 15:13:28 -0500

    Replaced our sphere player with a unity example character

--- Assets/Sample Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/Sample Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -3,32 +3,77 @@
 [RequireComponent(typeof(PlatformerCharacter2D))]
 public class Platformer2DUserControl : MonoBehaviour 
 {
+	public int playerNumber = 1;
 	private PlatformerCharacter2D character;
     private bool jump;
+	
+	string jumpInputName;
+	string moveInputName;
+	string aInputName;
+	string bInputName;
+	string xInputName;
+	string yInputName;
 
-
-	void Awake()
+	void Awake() 
 	{
 		character = GetComponent<PlatformerCharacter2D>();
+		DetermineInputs();
 	}
 
     void Update ()
     {
         // Read the jump input in Update so button presses aren't missed.
-        if (CrossPlatformInput.GetButtonDown("Jump"))
+        if (CrossPlatformInput.GetButtonDown(jumpInputName)) {
             jump = true;
+		}
+
+		CheckColourSwitch();
     }
 
-	void FixedUpdate()
+	void FixedUpdate ()
 	{
 		// Read the inputs.
 		bool crouch = Input.GetKey(KeyCode.LeftControl);
-		float h = CrossPlatformInput.GetAxis("Horizontal");
+		float h = CrossPlatformInput.GetAxis(moveInputName);
 
 		// Pass all parameters to the character control script.
-		character.Move( h, crouch , jump );
+		character.Move(h, crouch, jump);
 
         // Reset the jump input once it has been used.
 	    jump = false;
+	}
+
+	void DetermineInputs ()
+	{
+		var suffix = "Win";
+		
+		#if UNITY_STANDALONE_OSX
+		suffix = "Mac";
+		#endif
+		
+		jumpInputName = GenerateInputName("L_YAxis", suffix);
+		moveInputName = GenerateInputName("L_XAxis", suffix);
+		aInputName = GenerateInputName("A", suffix);
+		bInputName = GenerateInputName("B", suffix);
+		xInputName = GenerateInputName("X", suffix);
+		yInputName = GenerateInputName("Y", suffix);
+	}
+
+	string GenerateInputName (string prefix, string suffix)
+	{
+		return prefix + "_" + playerNumber + "_" + suffix;
+	}
+
+	void CheckColourSwitch ()
+	{
+		if (Input.GetButtonDown(aInputName)) {
+			LayerManager.ToggleLayer(gameObject, LayerColor.Green);
+		} else if (Input.GetButtonDown(bInputName)) {
+			LayerManager.ToggleLayer(gameObject, LayerColor.Red);
+		} else if (Input.GetButtonDown(xInputName)) {
+			LayerManager.ToggleLayer(gameObject, LayerColor.Blue);
+		} else if (Input.GetButtonDown(yInputName)) {
+			LayerManager.ToggleLayer(gameObject, LayerColor.Yellow);
+		}
 	}
 }
