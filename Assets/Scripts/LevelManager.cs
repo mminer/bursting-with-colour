81eb60078570c533d50b116a99ad4761454c94c5
brentgmitchell <brentgmitchell@outlook.com>
2014-01-25 18:25:06 -0500

    Added solid walls around the playspace.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,6 +7,9 @@ public class LevelManager : MonoBehaviour
 {
 	public Transform tile;
 	public int gridWidth = 20;
+	public int mapHeight = 100;
+	public int safeZoneWidth = 5;
+	public float chanceOfBlankTile = 0.2f;
 
 	void Start ()
 	{
@@ -14,21 +17,44 @@ public class LevelManager : MonoBehaviour
 		GenerateGround();
 		GenerateTiles();
 		InitializeLayerChildren();
+		if ((chanceOfBlankTile < 0f) || (chanceOfBlankTile > 1f)) chanceOfBlankTile = 0.2f;
 	}
 
 	void GenerateBorders()
 	{
 		//bottom ground
-		for (int x = -5; x < (gridWidth + 5); x++)
+		for (int x = -safeZoneWidth; x < gridWidth; x++)
 		{
 			CreateTile(LayerColor.Solid, x, -5);
 		}
 
-		//bottom walls
+		//bottom left wall
+		for (int y = -4; y < 0; y++)
+		{
+			CreateTile(LayerColor.Solid, -safeZoneWidth, y);
+		}
+
+		//bottomrightwall
+		for (int y = -5; y < 0; y++)
+		{
+			CreateTile(LayerColor.Solid, gridWidth, y);
+		}
 
 		//bottom overhang
+		for (int x = -4; x < 0; x++)
+		{
+			CreateTile(LayerColor.Solid, x, -1);
+		}
 
 		//tallwalls
+		for (int y = 0; y < mapHeight; y++)
+		{
+			CreateTile(LayerColor.Solid, -1, y);
+		}
+		for (int y = 0; y < mapHeight; y++)
+		{
+			CreateTile(LayerColor.Solid, gridWidth, y);
+		}
 	}
 
 	void GenerateGround ()
@@ -43,14 +69,14 @@ public class LevelManager : MonoBehaviour
 
 	void GenerateTiles ()
 	{
-		for (int y = 0; y < 100; y++) {
+		for (int y = 0; y < mapHeight; y++) {
 			for (int x = 0; x < gridWidth; x++) {
 				// Only create a new tile a specified percent of the time.
-				/*
-				if (Random.Range(0, 100) > 40) {
+
+				if (Random.Range(0f, 1f) < chanceOfBlankTile) {
 					continue;
 				}
-				*/
+
 
 				// Randomly decide which color to make tile.
 				var color = (LayerColor)Random.Range(0, LayerManager.layers.Count - 1);
