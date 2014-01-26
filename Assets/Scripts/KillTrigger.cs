35aa17130772929e197e016f1b577589bd28850c
brentgmitchell <brentgmitchell@outlook.com>
2014-01-25 23:25:29 -0500

    Adding player death stuffs.

---
+++ Assets/Scripts/KillTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillTrigger : MonoBehaviour
+{
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.gameObject.name.IndexOf("Player") >= 0)
+		{
+			col.gameObject.SendMessage("PlayerKilled");
+		}
+	}
+}
